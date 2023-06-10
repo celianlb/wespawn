dbd72701abb7409ab95ed87924fba86c091cf574
Célian LEBACLE <celianlebacle06@gmail.com>
2023-06-10 14:54:27 +0200

    hotjar

---
+++ note.md
@@ -0,0 +1,7 @@
+layout2 avec 'en savoir plus' 
+
+raccrocher les images à des div non vides
+
+tous les éléments en after : z-index négatif
+
+éléments naturels du bloc : z-index positif
--- src/components/Axeptio.tsx
+++ src/components/Axeptio.tsx
@@ -4,15 +4,27 @@ const Axeptio = () => {
     return (
         <Script strategy="afterInteractive" id="axeptio-hotjar">
         {`
+            <!-- Axeptio -->
             window.axeptioSettings = {
-            clientId: "6481e6ab8560303fec55a822",
-            cookiesVersion: "wespawn-fr",
-            };
-            (function(d, s) {
-            var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-            e.async = true; e.src = "//static.axept.io/sdk.js";
-            t.parentNode.insertBefore(e, t);
-            })(document, "script");
+                clientId: "6484704c2ad3bb7dcc849927",
+                cookiesVersion: "wespawn-fr",
+              };
+               
+              (function(d, s) {
+                var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+                e.async = true; e.src = "//static.axept.io/sdk.js";
+                t.parentNode.insertBefore(e, t);
+              })(document, "script");
+
+              <!-- Hotjar Tracking Code for https://www.wespawn.fr/ -->
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3528713,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `}
         </Script>
     );
