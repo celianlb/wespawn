0d7c18f38b50eb83e89c79ab1fcd9342cc2a0561
Célian LEBACLE <celianlebacle06@gmail.com>
2023-06-10 14:59:07 +0200

    twitter card opengraph

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
     <Html lang="fr">
       <title>WeSpawn</title>
       <meta name="description" content="WeSpawn est une plateforme qui vous aide à gérer l'e-sport" />
+      <meta name="twitter:card" content="summary"></meta>
+      <meta property="og:title" content="WeSpawn" />
+      <meta property="og:url" content="https://www.wespawn.fr/" />
+      <meta property="og:image" content="../../public/img/logo_wespawn.png" />
       <Head />
       <body>
         <Main />
