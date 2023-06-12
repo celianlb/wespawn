b0f1abd89e68b93b1748b7015e5b80b90997417c
Célian LEBACLE <celianlebacle06@gmail.com>
2023-06-12 15:51:03 +0200

    debug analytics

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,21 +2,18 @@ import Script from "next/script";
 
 const GoogleAnalytics = () => {
     return (
-        <Script strategy="afterInteractive" id="googleanalytics">
-        {`
-            <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-7FW6ZRB566"></script>
-            <script>
+        <><Script src="https://www.googletagmanager.com/gtag/js?id=G-7FW6ZRB566" strategy="afterInteractive" id="googleanalytics"></Script>
+        <Script strategy="afterInteractive">
+            {`
+            //Google tag (gtag.js)
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
+            
             gtag('config', 'G-7FW6ZRB566');
-            </script>
-            <!-- Google Optimize -->
-            <script src="https://www.googleoptimize.com/optimize.js?id=OPT-WLN22ZD"></script>
         `}
         </Script>
+        <Script src="https://www.googleoptimize.com/optimize.js?id=OPT-WLN22ZD" strategy="afterInteractive" id="googleoptimize"></Script></>
     );
     }
 
