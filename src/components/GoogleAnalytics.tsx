c6f3fee90eecc09fa3b20d63baaa42a567a2b9ee
Célian LEBACLE <celianlebacle06@gmail.com>
2023-06-12 12:38:30 +0200

    optimize analytics

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -5,15 +5,16 @@ const GoogleAnalytics = () => {
         <Script strategy="afterInteractive" id="googleanalytics">
         {`
             <!-- Google tag (gtag.js) -->
-            <script src="https://www.googleoptimize.com/optimize.js?id=OPT-WLN22ZD"></script>
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-7FW6ZRB566"></script>
             <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-7FW6ZRB566');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-7FW6ZRB566');
             </script>
+            <!-- Google Optimize -->
+            <script src="https://www.googleoptimize.com/optimize.js?id=OPT-WLN22ZD"></script>
         `}
         </Script>
     );
