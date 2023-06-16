c34c1b03c95477d3322919138f3ecaf87e34a773
Célian LEBACLE <celianlebacle06@gmail.com>
2023-06-16 11:06:46 +0200

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default function Document() {
       <meta property="og:title" content="WeSpawn" />
       <meta property="og:url" content="https://www.wespawn.fr/" />
       <meta property="og:image" content="../../public/img/logo_wespawn.png" />
+      <meta name="google-site-verification" content="YPMqr6gttIsVaFXw76yNbBvKWSOIglMU1LBiVkd8Ufk" />
       <Head>
       </Head>
       <body>
